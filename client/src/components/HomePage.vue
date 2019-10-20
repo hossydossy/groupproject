3eed3f22db2530fd6f827570907cb2a76fe435f1
Rebecca Still <beckystill@hotmail.com>
2019-10-20 21:05:27 +0100

    test

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="main-container">
     <!-- <nav-bar></nav-bar> -->
-    <h1>Countries of the World</h1>
-    <h2>Educational App for Children</h2>
+    <h1>Explore the World</h1>
+    <h2>Play and Learn</h2>
     <call-to-action></call-to-action>
   </div>
 </template>
