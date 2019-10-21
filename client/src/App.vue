65e2af58f3f1bd60bd21f1addf82a84a4f6bb6dd
gerald <geraldine.antonelli@gmail.com>
2019-10-21 23:08:57 +0100

    feat: changes and fix

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,10 @@
 <template lang="html">
 <div class="bg">
   <home-page v-if="!menuOption"></home-page>
-  <play v-if="menuOption === 'play'" :countries="countries" v-on:click="loadCountries('play')"></play>
-  <picture-game v-if="menuOption === 'playPG'" ></picture-game>
+  <play v-if="menuOption === 'play'"></play>
+  <picture-game v-if="menuOption === 'playPG'"></picture-game>
   <map-game v-if="menuOption === 'playMG'" :countries="countries"></map-game>
   <learn v-if="menuOption === 'learn'" :countries="countries"></learn>
-  <!-- <learn-country></learn-country> -->
-  <!-- <learn-tourist-attractions v-if="learnInitialised && touristAttractions && !learnCountries"></learn-tourist-attractions> -->
-
-  <game-1 v-if="menuOption == 'game'" :countries="countries"/>
-  <learn-country v-if="menuOption == 'learn'" :countries="countries"/>
-  <button type="button" name="button" v-on:click="loadCountries('learn')">Learn</button>
-  <button type="button" name="button" v-on:click="loadCountries('game')">Game</button>
 
 </div>
 </template>
@@ -29,21 +22,12 @@ import Play from './components/Play.vue';
 import PictureGame from './components/PictureGame.vue';
 import MapGame from './components/MapGame.vue';
 import LearnCountry from './components/LearnCountry.vue';
-import Game1 from './components/Game1.vue';
 
 export default {
   name: 'app',
   data() {
     return {
-      // playInitialised: false,
-      // playPG: false,
-      // playMG: false,
-      // playBG: false,
-      // learnInitialised: false,
-      // learnCountries: false,
-      // touristAttractions: false,
       countries: [],
-      // selectedCountry: null,
       country: null,
       menuOption: null,
     };
@@ -60,21 +44,19 @@ export default {
     'picture-game': PictureGame,
     'map-game': MapGame,
     "learn-country": LearnCountry,
-    "game-1": Game1
   },
   methods: {
     loadCountries(option) {
       this.menuOption = option;
-      // fetch(`http://localhost:3000/api/countries`)
-      //   .then(response => response.json())
-      //   .then(apiResponse => this.countries = apiResponse)
+      fetch(`http://localhost:3000/api/countries`)
+        .then(response => response.json())
+        .then(apiResponse => this.countries = apiResponse)
     }
   },
   mounted() {
-
     fetch(`http://localhost:3000/api/countries`)
       .then(response => response.json())
-      .then(apiResponse => this.countries = apiResponse)
+      .then(apiResponse => this.countries = apiResponse);
 
     eventBus.$on('go-to', (option) => {
       this.menuOption = option;
