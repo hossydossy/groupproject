92faba029ae171d469bc3353dcf6cb33133290f4
gerald <geraldine.antonelli@gmail.com>
2019-10-22 14:16:56 +0100

    background and changes

--- client/src/components/Play.vue
+++ client/src/components/Play.vue
@@ -41,15 +41,13 @@ h2 {
   -webkit-box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
   -moz-box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
   box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
+
 }
 #games-container {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  border: 1px solid #dfe1e5;
-  border-radius: 8px;
-  -webkit-box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
-  -moz-box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
-  box-shadow: 15px 14px 37px -19px rgba(0,0,0,0.75);
 }
+
+
 </style>
